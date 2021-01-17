02319452f7cb3ac3e0f582fe2c2e5b44053e501d
baduhai <baduhai@github.com>
2021-01-17 13:58:12 +0100

    Added command-line arguments

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
-
 #include <stdio.h>
+#include <stdint.h>
 #include "proxy.h"
 
 #define MAX_CLIENTS 40
 #define TIMEOUT 15 // seconds
 #define CLIENT_PORT 19132 // port that the Xbox One attempts to connect to, do not change
-#define SERVER_PORT 19132
-#define SERVER_ADDR "exampleserver.com"
 
 int main(int argc, char* argv[]) {
-  int ret;
+  int ret, timeout, max_clients;
+  uint32_t server_port;
 
-  ret = start_proxy(CLIENT_PORT, SERVER_PORT, SERVER_ADDR, TIMEOUT, MAX_CLIENTS, NULL, NULL);
+  if(argc < 3){
+    printf("Not enough arguments supplied.\nUsage:\nbedrock-proxy <server-address> <server-port> <max_clients> <timeout>\nServer address and port arguments are required.\nMax clients and timeout(in seconds) arguments are optional and default to 40 and 15 respectively.\nExample:\nbedrock-proxy exampleserver.com 19132\n");
+    return(0);
+  }
+  else if(argc == 3) {
+    server_port = (unsigned)atoi(argv[2]);
+    max_clients = MAX_CLIENTS;
+    timeout = TIMEOUT;
+  }
+  else if(argc == 4) {
+    server_port = (unsigned)atoi(argv[2]);
+    max_clients = atoi(argv[3]);
+    timeout = TIMEOUT;
+  }
+  else if(argc == 5) {
+    server_port = (unsigned)atoi(argv[2]);
+    max_clients = atoi(argv[3]);
+    timeout = atoi(argv[4]);
+  }
+  else {
+    printf("Too many arguments supplied.\n");
+    return(0);
+  }
+  ret = start_proxy(CLIENT_PORT, server_port, argv[1], timeout, max_clients, NULL, NULL);
   if(ret) {
     printf("Error! %i\n", ret);
   }
